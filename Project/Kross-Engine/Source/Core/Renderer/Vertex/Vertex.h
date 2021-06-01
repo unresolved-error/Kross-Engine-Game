3d960cbf68949219b95219313e3da7d834d306c3
Deklyn Palmer <deklyn.paul.palmer@outlook.com>
2021-06-01 17:12:15 +1000

    Added Vertex Parent

---
+++ Project/Kross-Engine/Source/Core/Renderer/Vertex/Vertex.h
@@ -0,0 +1,18 @@
+/*
+ *  Author: Deklyn Palmer.
+ *  Editors:
+ *      - Deklyn Palmer.
+ */
+
+#pragma once
+
+#include "../../Core.h"
+
+namespace Kross
+{
+	struct KROSS_API Vertex {};
+}
+
+#include "SpriteVertex.h"
+#include "TextVertex.h"
+#include "WaterVertex.h"
