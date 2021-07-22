c314ae565dcfde282fc5f47434d54ee96333b92e
Chris Deitch <chrisdeitch@hotmail.com>
2021-07-22 11:32:46 +1000

    object editor blockout.

---
+++ Project/Kross-Engine/Source/Core/Editor/Windows/ObjectEditor.cpp
@@ -0,0 +1,16 @@
+#include "ObjectEditor.h"
+
+namespace Kross {
+
+	void SceneHierarchy::Attach()
+	{
+	}
+
+	void SceneHierarchy::SetFlags()
+	{
+	}
+
+	void SceneHierarchy::OnStart()
+	{
+	}
+}
---
+++ Project/Kross-Engine/Source/Core/Editor/Windows/ObjectEditor.h
@@ -0,0 +1,23 @@
+/*
+ *  Author: Chris Deitch.
+ *  Editors:
+ *      - Chris Deitch.
+ */
+
+#pragma once
+#include "../../Core.h"
+#include "../EditorWindow.h"
+
+
+namespace Kross
+{
+    class KROSS_API SceneHierarchy : public EditorWindow 
+    {
+    public:
+
+    private:
+
+    protected:
+
+    };
+}
