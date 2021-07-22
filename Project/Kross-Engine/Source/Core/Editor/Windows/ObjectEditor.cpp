b9bc7f40b86e9cef66a398ffa1f72e44bf120668
Chris Deitch <chrisdeitch@hotmail.com>
2021-07-22 11:47:33 +1000

    obj edit

--- Project/Kross-Engine/Source/Core/Editor/Windows/ObjectEditor.cpp
+++ Project/Kross-Engine/Source/Core/Editor/Windows/ObjectEditor.cpp
@@ -4,13 +4,18 @@ namespace Kross {
 
 	void ObjectEditor::Attach()
 	{
+		
 	}
 
 	void ObjectEditor::SetFlags()
 	{
+		m_WindowFlags = ImGuiWindowFlags_::ImGuiWindowFlags_AlwaysAutoResize;
 	}
 
 	void ObjectEditor::OnStart()
 	{
+		SetFlags();
+		m_Title = "object editor panel";
+
 	}
 }
